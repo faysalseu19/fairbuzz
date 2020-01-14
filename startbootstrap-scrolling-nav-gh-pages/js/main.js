bef0d653e0a92ee081abac285eab07d84ac8c7ca
Md. Faysal Mia <faysalmia7292@gmail.com>
2020-01-14 14:54:06 +0600

    initial commit

---
+++ startbootstrap-scrolling-nav-gh-pages/js/main.js
@@ -0,0 +1,207 @@
+(function ($) {
+    "use strict";
+    
+    // sticky
+    $(window).on('scroll',function() {    
+      var scroll = $(window).scrollTop();
+      if (scroll < 245) {
+       $(".navbar").removeClass("scroll-header");
+      }else{
+       $(".navbar").addClass("scroll-header");
+      }
+     });
+     
+     
+       var nav_offset_top = $('navbar').height()+10; 
+       /*-------------------------------------------------------------------------------
+       Navbar 
+     -------------------------------------------------------------------------------*/
+   
+     //* Navbar Fixed  
+       function navbarFixed(){
+           if ( $('.navbar').length ){ 
+               $(window).scroll(function() {
+                   var scroll = $(window).scrollTop();   
+                   if (scroll >= nav_offset_top ) {
+                       $(".navbar").addClass("navbar_fixed");
+                   } else {
+                       $(".navbar").removeClass("navbar_fixed");
+                   }
+               });
+           };
+       };
+       navbarFixed();
+
+    $('.services-active').slick({
+		dots: true,
+		arrows: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
+    
+    $('.cases-active').slick({
+		dots: false,
+		arrows: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+		slidesToScroll: 1,
+		prevArrow: '<button type="button" class="slick-prev"><i class="far fa-arrow-alt-circle-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="far fa-arrow-alt-circle-right"></i></button>',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
+    
+    // Testimonial slick active
+    $('.testimonial-active').slick({
+      dots: false,
+      arrows: true,
+          infinite: false,
+          speed: 300,
+          slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: '<button type="button" class="slick-prev"><i class="far fa-arrow-alt-circle-left"></i></button>',
+      nextArrow: '<button type="button" class="slick-next"><i class="far fa-arrow-alt-circle-right"></i></button>',
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+      });
+    // brand  slick active
+    $('.brand-active').slick({
+      dots: false,
+      arrows: false,
+          infinite: false,
+          speed: 300,
+          slidesToShow: 6,
+      slidesToScroll: 1,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 6,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: false
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+      });
+
+  // nice select
+  $('select').niceSelect();
+
+	//   Magnific PopUp
+	$('.view-video').magnificPopup({
+		type: 'iframe'
+		// other options
+	  });
+
+
+})(jQuery);	
